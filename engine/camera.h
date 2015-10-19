a8397ec9b72bb87e10874ba3702e2ba015ff3afb
LimoTau <timolaudi@gmail.com>
2015-10-19 14:29:13 +0200

    - Added documentation to node.h and documentation main page

--- engine/camera.h
+++ engine/camera.h
@@ -18,14 +18,14 @@ class camera {
                glm::mat4 projection = glm::perspective(45.0f, 16.0f / 9.0f, 0.1f, 100.0f));
         camera(glm::mat4 view, glm::mat4 projection);
         ~camera();
-        void set_position(float arg_x, float arg_y, float arg_z);///< Places the camera at the specified point in world space.
-        void set_position(glm::vec3 arg);
-        void point_at(float arg_x, float arg_y, float arg_z);///< Rotates the camera to face the specified point in world space.
-        void point_at(glm::vec3 arg);
+        void set_position(float arg_x, float arg_y, float arg_z);///< Places the camera at the specified point in world space. If the camera has a parent node the position is set relative to that node.
+        void set_position(glm::vec3 arg);;///< See member set_position(float arg_x, float arg_y, float arg_z).
+        void point_at(float arg_x, float arg_y, float arg_z);///< Rotates the camera to face the specified point in world space. If the camera has a parent node the target point is set relative to that node.
+        void point_at(glm::vec3 arg);///< See member point_at(float arg_x, float arg_y, float arg_z).
         void setup(glm::vec3 arg_pos, glm::vec3 arg_point);///< Combines the functions set_position() and point_at()
         const glm::mat4 get_view() const;///< Returns the view matrix of the camera.
         const glm::mat4 &get_projection() const;///< Returns the projection matrix of the camera.
-        void set_parent(node *new_parent);
+        void set_parent(node *new_parent);///< Assigns a parent node to the camera. All other coordinates will be set relative to the parent node.
 
     private:
     	node *parent;
--- engine/node.h
+++ engine/node.h
@@ -1,5 +1,7 @@
-//Class containing a group of mesh objects that can be transformed and rendered as a group
-//Can have other node objects as children or parents
+/**
+Class containing a group of mesh objects that can be transformed and rendered as a group.
+Can have other node objects as children or parents.
+*/
 
 #ifndef NODE_H
 #define NODE_H
@@ -14,27 +16,27 @@
 
 class node {
     public:
-        node();
-        node(const std::string &scene_path);
-        ~node();
-        void load_model(const std::string &path);
-        bool load_scene(const std::string &path);
-        void set_material(std::shared_ptr<material> new_material);
-        void place(float x, float y, float z);
-        void move(float x, float y, float z);
-        void move_relative(float x, float y, float z);
-        void set_orientation(float x, float y, float z);
-        void set_scale(float x, float y, float z);
-        void set_scale(float new_scale);
-        void append_node(const std::string &file_path);
-        void append_node(node *new_child);
-        void set_parent(node *new_parent);
-        bool remove_child(node *child);
-        glm::mat4 get_node_matrix() const;
-        void render(glm::mat4 view_matrix) const;
-        void render(glm::mat4 parent_matrix, glm::mat4 view_matrix);
-        std::vector< node*> enumerate();
-        const std::vector< mesh *> &get_models();
+        node();///< Default constructor.
+        node(const std::string &scene_path);///< Constructor loading all meshes from a file and appending them to the node.
+        ~node();///< Default destructor.
+        void load_model(const std::string &path);///< Loads the first mesh in the specified file and appends it to the node.
+        bool load_scene(const std::string &path);///< Loads all meshes from the specified file and appends them to the node.
+        void set_material(std::shared_ptr<material> new_material);///< Sets the material for all nodes and meshes appended to this node.
+        void place(float x, float y, float z);///< Specifies the node location relative to its parent node.
+        void move(float x, float y, float z);///< Moves the node relative to its parent node.
+        void move_relative(float x, float y, float z);///< Moves the node relative to its parent considering the node's rotation.
+        void set_orientation(float x, float y, float z);///< Sets the node's rotation relative to its parent.
+        void set_scale(float x, float y, float z);///< Sets the node's scale.
+        void set_scale(float new_scale);///< See member set_scale(float x, float y, float z).
+        void append_node(const std::string &file_path);///< Basically append_node(new node(file_path)).
+        void append_node(node *new_child);///< Appends the given node as its child.
+        void set_parent(node *new_parent);///< Set the node's parent.
+        bool remove_child(node *child);///< Removes a child node from this node.
+        glm::mat4 get_node_matrix() const;///< Returns the transformation matrix affecting all of the node's children. It includes the transformation of it's parents.
+        void render(glm::mat4 view_matrix) const;///< Renders the whole node with all of its descendants.
+        void render(glm::mat4 parent_matrix, glm::mat4 view_matrix);///< Renders the whole node with special transformation and view matrices.
+        std::vector< node*> enumerate();///< Returns a list of itself and all its child nodes.
+        const std::vector< mesh *> &get_models();///< Returns a list of all mesh objects attached to this node.
 
     private:
         node *parent_node;
